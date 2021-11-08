5a80461d04a1640987a936249a08898911be9801
Christine Contreras <christineec23@yahoo.com>
2021-11-08 10:36:41 -0600

    working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,8 @@
 class SessionsController < ApplicationController
   
+  # In this method, we're setting values on the session hash and the cookies hash, and serializing them in the response so we can view their values in the browser.
   def index
+    # ||= syntax in Ruby, it's a shorthand way to assign a value if the current value is nil or false. So if session[:session_hello] has not already been assigned a value, it will be assigned a value of "World". Otherwise, it won't get assigned a new value.
     session[:session_hello] ||= "World"
     cookies[:cookies_hello] ||= "World"
     render json: { session: session, cookies: cookies.to_hash }
